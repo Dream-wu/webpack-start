331563ec650a8c0b44b289301dd81ef817561966
Dream-wu <532142910>
2018-10-24 10:21:42 +0800

    增加css

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -1,16 +1,45 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); 
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+function resolve(dir) {
+  return path.join(__dirname, '..', dir)
+}
 module.exports = {
   entry: {
     main: './src/index.js'
   },
   output: {
     path: path.resolve(__dirname, '../dist'),
-    filename: '[name].[chunkhash].bundle.js'
+    filename: '[name].[hash:5].bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.json'],
+    alias: {
+      '@': resolve('src')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          //MiniCssExtractPlugin.loader,
+          "css-loader"
+        ]
+      }
+    ]
   },
   plugins: [
+    // new BrowserSyncPlugin({
+    //   host: 'localhost',
+    //   port: 9000,
+    //   proxy: 'http://localhost:9060'
+    // }, {
+    //   reload: false
+    // }),
     new CleanWebpackPlugin(
       ['dist'], 
       {
@@ -20,6 +49,22 @@ module.exports = {
     ),
     new HtmlWebpackPlugin({ 
       template: './src/index.html' 
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
     })
-  ]
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /.css$/,
+          chunks: 'all',    // merge all the css chunk to one file
+          enforce: true
+        }
+      }
+    }
+  }
 }
