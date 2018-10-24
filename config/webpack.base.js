7086da7d737329d1ce09bb92450fb97a2c4b7ec7
Dream-wu <532142910>
2018-10-24 19:08:59 +0800

    webpack-dev-server

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -32,6 +32,18 @@ webpackBase = {
       }
     ]
   },
+  devServer: {
+    // port:3000,
+    // hot:true,
+    before(app) {
+      app.get("/api/test",(req,res) => {
+        res.json({
+          code:200,
+          message:"hello word"
+        });
+      })
+    }
+  },
   plugins: [
     new CleanWebpackPlugin(
       ['dist'], 
